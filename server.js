d59cd0c55b44914a3f3bc2f92476d4abe78b644f
Santisaik <e.retoque@gmail.com>
2026-04-14 11:17:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const PROJECT_ID = process.env.GCP_PROJECT_ID;
 const LOCATION = process.env.GCP_LOCATION || 'europe-west1';
 const MODELS = [
   'gemini-3.1-flash-image-preview',
-  'gemini-2.5-flash-image'
 ];
 
 // Auth - se configura automáticamente con GOOGLE_APPLICATION_CREDENTIALS o con la variable de entorno
